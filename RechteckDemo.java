d0060e72890798570865781b89cc428370366351
Bianca <s52340@edu.campus02.at>
2022-09-25 23:24:11 +0200

    VariableDemo + RechteckDemo

--- RechteckDemo.java
+++ RechteckDemo.java
@@ -1,2 +1,17 @@
 public class RechteckDemo {
+    public static void main(String[] args) {
+        byte breite = 23;
+        byte laenge = 57;
+        int umfang;
+        int flaeche;
+
+        umfang = (2*breite) + (2*laenge);
+
+        flaeche =  breite * laenge;
+
+        System.out.println("Wir haben ein Rechteck mit der Breite: " + breite + " und der Länge: " + laenge);
+        System.out.println("Der Umfang des Rechtecks ist: " + umfang);
+        System.out.println("Die Fläche des Rechtecks lautet: " + flaeche);
+
+    }
 }
