f051222aefce79e29774bb21f23b238aaedc7b43
Bianca <s52340@edu.campus02.at>
2022-09-25 23:14:06 +0200

    VariableDemo

---
+++ VariableDemo.java
@@ -0,0 +1,8 @@
+public class VariableDemo {
+    public static void main(String[] args) {
+        String Vorname = "Max ";
+        String Nachname = "Mustermann";
+
+        System.out.println("Der Name lautet: " + Vorname + Nachname);
+    }
+}
